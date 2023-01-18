d22f424eeee49c20fc802b90852dc2241d41a92f
KimJacobus <jacobus.kim@gmail.com>
2023-01-19 00:31:52 +0100

    add all

---
+++ DEV.md
@@ -0,0 +1,8 @@
+# Calculator 
+
+
+## Notes on development :
+
+### Things to fix :
+
+- (Please enable a CSS nesting plugin *before* Tailwind in your configuration). have to add plugins in postcss apparently. 
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,html,css}",
+  ],
+  theme: {
+
+    extend: {
+
+    fontFamily: {
+      primary: 'dosis' 
+    }, 
+    fontFamily: {
+      primary: 'sofia' 
+    }, 
+      
+    },
+  },
+  plugins: [
+
+  ],
+}
