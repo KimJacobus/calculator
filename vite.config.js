1558040dfbe30f5ecb976adf0a7200bc83f300aa
KimJacobus <jacobus.kim@gmail.com>
2023-01-19 02:02:13 +0100

    fonts up

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
-  base: "",
+  base: "live",
   build: {
     rollupOptions: {
       input: {
